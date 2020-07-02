77a2522fb725943b1bae3748be0109b4b88fbaf4
VolatilePulse <shearer.zachary@gmail.com>
2020-07-01 20:12:27 -0400

    Create Multipliers class
    Additional testing and calculations for baseLevel calculation

---
+++ src/js/cls/class.ts
@@ -0,0 +1,26 @@
+import IA, { Interval } from 'interval-arithmetic'
+
+export class Multipliers {
+   B: Interval;
+   Iw: Interval;
+   Id: Interval;
+   Ta: Interval;
+   Tm: Interval;
+   Ib: Interval;
+
+   constructor(m: Interval[]) {
+      this.B = m[0];
+      this.Iw = IA.mul(m[1], m[6]);
+      this.Id = IA.mul(m[2], m[7]);
+
+      this.Ta = m[3];
+      if (this.Ta > IA.ZERO)
+         this.Ta = IA.mul(this.Ta, m[8]);
+
+      this.Tm = m[4];
+      if (m[4] > IA.ZERO)
+         this.Tm = IA.mul(this.Ta, m[9]);
+      
+      this.Ib = IA.mul(m[5], m[10])
+   }
+}
--- src/js/extractor.test.ts
+++ src/js/extractor.test.ts
@@ -1,157 +1,93 @@
-import IA from 'interval-arithmetic'
+import IA from 'interval-arithmetic';
 import { ExtractLevelsFromTorpor } from './extractor';
-// wildTamedBred = 1: wild, 2: tamed, 3: bred
-// multipliers = [B, Iw, Id, Ta, Tm, IwM, IdM, TaM, TmM, IBM]
+import { Multipliers } from './cls/class';
 
-// Extract creature base & domestic levels
-// function ExtractLevels(level: number, wildTamedBred: number, torpor: Interval, imprint: Interval, multipliers: Interval[]): any {
+const multipliers = Array(11).fill(IA.ZERO);
+multipliers[0] = new IA(100);
+const mults: Multipliers = new Multipliers(multipliers);
 
-// }
-
-test('ExtractLevels() works with a basic test case', () => {
-   let torpor = new IA(100);
-   let imprint = IA.ZERO;
+describe('ExtractLevels', () => {
+   it('works with a basic test case', () => {
+      const torpor = new IA(100);
+      const imprint = IA.ZERO;
+      const m = mults;
    
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   const result = ExtractLevelsFromTorpor(1, torpor, IA.ONE, imprint, multipliers, false)
-
-   expect(result).toEqual([[1,0]])
-})
-
-test('ExtractLevels() works with a wild level of 50', () => {
-   let torpor = new IA(149);
-   let imprint = IA.ZERO;
+      const result = ExtractLevelsFromTorpor(1, torpor, IA.ONE, imprint, m, false);
    
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   const result = ExtractLevelsFromTorpor(50, torpor, IA.ONE, imprint, multipliers, false)
-
-   expect(result).toEqual([[50,0]])
-})
-
-test('ExtractLevels() works with a tamed level of 50 with Ta', () => {
-   let torpor = new IA(149.5);
-   let imprint = IA.ZERO;
+      expect(result).toEqual([[1,0]]);
+   })
    
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      new IA(0.5),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ONE,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   const result = ExtractLevelsFromTorpor(50, torpor, IA.ONE, imprint, multipliers, false)
-
-   expect(result).toEqual([[50,0]])
-})
-
-test('ExtractLevels() works with a tamed level of 75 with Ta and has 25 domestic levels', () => {
-   let torpor = new IA(149.5);
-   let imprint = IA.ZERO;
+   it('works with a wild level of 50', () => {
+      const torpor = new IA(5000);
+      const imprint = IA.ZERO;
+      const m = mults;
    
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      new IA(0.5),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ONE,
-      IA.ZERO,
-      IA.ZERO,
-   ];
+      const result = ExtractLevelsFromTorpor(50, torpor, IA.ONE, imprint, m, false);
+   
+      expect(result).toEqual([[50,0]]);
+   })
+   
+   it('works with a tamed level of 50 with Ta', () => {
+      const torpor = new IA(5000.5);
+      const imprint = IA.ZERO;
+      const m = mults;
+      m.Ta = new IA(0.5);
+   
+      const result = ExtractLevelsFromTorpor(50, torpor, IA.ONE, imprint, m, false);
+   
+      expect(result).toEqual([[50,0]]);
+   })
+   
+   it('works with a tamed level of 75 with Ta and has 25 domestic levels', () => {
+      const torpor = new IA(5000.5);
+      const imprint = IA.ZERO;
+      const m = mults;
+   
+      const result = ExtractLevelsFromTorpor(75, torpor, IA.ONE, imprint, m, false);
+   
+      expect(result).toEqual([[50,25]]);
+   })
+   
+   it('works with a bred level of 50 with 100% imprint', () => {
+      const torpor = new IA(6000.5);
+      const imprint = IA.ONE;
+      const m = mults;
+      m.Ta = new IA(0.5);
+      m.Ib = new IA(0.2);
+   
+      const result = ExtractLevelsFromTorpor(50, torpor, IA.ONE, imprint, m, false);
+   
+      expect(result).toEqual([[50,0]]);
+   })
+   
+   it('works with a bred level of 200 with 100% imprint and narrow IB range', () => {
+      const torpor = new IA(102.45, 102.549999);
+      const imprint = new IA(0.995, 1);
+      const m = mults;
+      m.Ta = new IA(0.5);
+      m.Ib = new IA(0.2 * 0.0001);
+   
+      const result = ExtractLevelsFromTorpor(201, torpor, new IA(0.0001), imprint, m, false);
+   
+      expect(result).toEqual([[196,5], [197,4], [198,3], [199,2], [200,1], [201, 0]]);
+   })
 
-   const result = ExtractLevelsFromTorpor(75, torpor, IA.ONE, imprint, multipliers, false)
+   it('throws an Error when Torpor can be leveled', () => {
+      const m = mults;
 
-   expect(result).toEqual([[50,25]])
-})
+      expect(() => ExtractLevelsFromTorpor(1, new IA(100), IA.ONE, IA.ZERO, m, true)).toThrow(Error);
+   })
+   
+   it('throws an Error when Torpor has a Tm', () => {
+      const m = mults;
+      m.Tm = IA.ONE;
 
-test('ExtractLevels() throws exception when Torpor can be leveled', () => {
-   let torpor = new IA(100);
-   let imprint = IA.ZERO;
+      expect(() => ExtractLevelsFromTorpor(1, new IA(100), IA.ONE, IA.ZERO, m, true)).toThrow(Error);
+   })
+   
+   it('throws an Error when Torpor increase per level is IA.ZERO', () => {
+      const m = mults;
 
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   expect(() => ExtractLevelsFromTorpor(1, torpor, IA.ONE, imprint, multipliers, true)).toThrow(Error);
-})
-
-test('ExtractLevels() throws exception when Torpor has a Tm', () => {
-   let torpor = new IA(100);
-   let imprint = IA.ZERO;
-
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ONE,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   expect(() => ExtractLevelsFromTorpor(1, torpor, IA.ONE, imprint, multipliers, true)).toThrow(Error);
-})
-
-test('ExtractLevels() throws exception when Torpor increase per level is IA.ZERO', () => {
-   let torpor = new IA(100);
-   let imprint = IA.ZERO;
-
-   let multipliers = [
-      new IA(100),
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ONE,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-      IA.ZERO,
-   ];
-
-   expect(() => ExtractLevelsFromTorpor(1, torpor, IA.ZERO, imprint, multipliers, true)).toThrow(Error);
+      expect(() => ExtractLevelsFromTorpor(1, new IA(100), IA.ZERO, IA.ZERO, m, true)).toThrow(Error);
+   })
 })
